63e4572e5efa2dc466ae7a9eaa5037697cddf3cd
512642 <72399788+512642@users.noreply.github.com>
2021-05-05 16:30:26 +0100

    sfsfffsegsefsegseg

---
+++ Final piece/Assets/Scripts/RockSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawn : MonoBehaviour
+{
+    public GameObject spawner;
+    public GameObject rockPrefab;
+    private GameObject rockInstance;
+
+
+        void Spawn()
+    {
+        rockInstance = Instantiate(rockPrefab);
+        
+
+    }
+
+}

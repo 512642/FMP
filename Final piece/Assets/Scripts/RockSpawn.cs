92931d97119b083ae2f4b1fdcac458d2f9131dbe
512642 <72399788+512642@users.noreply.github.com>
2021-05-10 14:05:00 +0100

    rocks fall good

--- Final piece/Assets/Scripts/RockSpawn.cs
+++ Final piece/Assets/Scripts/RockSpawn.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class RockSpawn : MonoBehaviour
 {
     public Vector3 offset;
-    private float time = 0.0f;
-    private float timeCounter = 2.0f;
     public GameObject spawner;
     public GameObject rockPrefab;
     private GameObject rockInstance;
@@ -17,16 +15,7 @@ public class RockSpawn : MonoBehaviour
 
     }
 
-    void update()
-    {
-        time += Time.deltaTime;
 
-        if(time > timeCounter)
-        {
-            time = 0.0f;
-            transform.position = player.position + offset;
-        }
-    }
 
         void Spawn()
     {

782713a550033f0dc131134de27cd82f6af05dbb
512642 <72399788+512642@users.noreply.github.com>
2021-05-12 15:33:10 +0100

    timer started

---
+++ Final piece/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public float score = 0;
+    Text inGameScore;
+    Text finalScoreText;
+    private float finalScore = 0;
+
+
+    public void UpdateScore()
+    {
+        inGameScore = GameObject.Find("InGameScore").GetComponent<Text>();
+        score += Time.deltaTime;
+        print(score);
+    }
+    /*public void FinalScore()
+    {
+        finalScoreText = GameObject.Find("ScoreDisplay").GetComponent<Text>();
+        score = 
+
+
+        finalScoreText.text = "Final Score: " + finalScore;
+    }*/
+}

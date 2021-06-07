04f10186bd1377c476670afb4ce8e3c0e8a157a7
512642 <72399788+512642@users.noreply.github.com>
2021-06-07 11:23:15 +0100

    suvival mode added

--- Final piece/Assets/Scripts/UIrelated/NextLevelScript.cs
+++ Final piece/Assets/Scripts/UIrelated/NextLevelScript.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class NextLevelScript : MonoBehaviour
 {
 
+ GameObject Level1Layout = GameObject.Find("Level 1 Layout");
+ GameObject Level2Layout = GameObject.Find("Level 2 Layout");
+
 public void MainMenuReturn()
 {
     SceneManager.LoadScene("Main Menu");
@@ -15,6 +18,11 @@ public void RetryLevel1()
     SceneManager.LoadScene("Level 1");
 }
 
+public void PlaySurvival()
+{
+    SceneManager.LoadScene("Survival");
+
+}
 
 
 }
